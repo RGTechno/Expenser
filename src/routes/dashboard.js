f1587762c271b1f75153cfc5d185855e0ed4e124
RGTechno <>
2020-05-31 19:55:09 +0530

    Transaction functionality

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,4 +1,5 @@
 const { Router } = require("express")
+const { Transactions } = require("../database/db")
 
 const route = Router()
 
@@ -9,6 +10,17 @@ route.get("/",(req,res)=>{
     else{
         res.redirect("/account/login")
     }
+})
+
+route.post("/", async (req,res)=>{
+
+    console.log(req.body)
+    const transaction = await Transactions.create({
+        price : req.body.inpPrice,
+        title : req.body.inpTitle,
+        userId : req.user.id
+    })  
+    return transaction  
 })
 
 module.exports = {
