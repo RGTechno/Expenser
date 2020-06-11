8262afb2949f62a24afa4f25fd906e7f177111ef
RGTechno <>
2020-06-11 13:41:01 +0530

    added date

--- src/public/app/dashboard.js
+++ src/public/app/dashboard.js
@@ -3,14 +3,15 @@ $(() => {
     $.get("/dashboard/transactions", (transactions) => {
         for (let t of transactions) {
             totalexpense+=t.price
-            $("#ulexpense").append(
+            $("#ulexpense").prepend(
                 `<li class="list-group-item" id="listitem">
                     <div class="container m-1 p-1 col">
                         <div class="row">
-                            <p class="font-weight-bolder border border-dark rounded-pill p-3 col-8" id="listprice">RS. ${t.price}/=</p>
+                            <p class="font-weight-bolder border border-dark rounded-pill p-3 col-sm-8 col-md-8" id="listprice">RS. ${t.price}/=</p>
                         </div>
                         <div class="row">
-                            <p class="font-weight-bold p-3 col" id="listtitle">${t.title}</p>
+                            <p class="font-weight-bold p-3 col-sm-10 col-md-10" id="listtitle">${t.title}</p>
+                            <small class="col-sm-2 col-md-2 p-3">${t.createdAt.substr(0,10)}</small>
                         </div>                    
                     </div>
                 </li>`
